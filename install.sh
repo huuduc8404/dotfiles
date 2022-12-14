f05555d8a7784e9827219cba48ba5212312e84c4
huuduc8404 <huuduc8404@gmail.com>
2022-12-14 14:35:21 +0000

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-chsh -s $(which zsh)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+sed 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|g' -i ~/.zshrc
+cp -f .zshrc ~/.zshrc
+cp -f .p10k.zsh ~/.p10k.zsh
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
