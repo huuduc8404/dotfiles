1bd0f42a35ef9d43736821f185f6a09e71f1dfb2
huuduc8404 <huuduc8404@gmail.com>
2022-12-15 04:32:08 +0000

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -x
 rm -rf /home/gitpod/.oh-my-zsh
-cd /home/gitpod/.dotfiles
-RUNZSH="no" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+# cd /home/gitpod/.dotfiles
+cd /workspace/dotfiles
+RUNZSH="no" | ./ohmyzsh.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 
