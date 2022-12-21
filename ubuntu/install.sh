1415dd8f3c03f8cb1aac826455ab1148030003bb
Ubuntu <huuduc8404@gmail.com>
2022-12-21 17:40:55 +0000

    a

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -13,7 +13,15 @@ git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-$
 
 cp -f .zshrc ~/.zshrc
 cp -f .p10k.zsh ~/.p10k.zsh
+
+
+sudo apt install unzip -y
+set -x
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-brew install -q kubectx
-sudo aws/install
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 
