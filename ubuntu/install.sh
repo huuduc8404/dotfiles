0aa1592af77349db4e6a8a2e5bf463686450746e
huuduc8404 <huuduc8404@gmail.com>
2022-12-21 16:31:56 +0000

    a

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -x
 # rm -rf ~/.oh-my-zsh
+sudo apt install python3-pip
+pip3 install apt-select
+~/.local/bin/apt-select -C VN
 wget -O ohmyzsh.sh  https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sudo apt install zsh -y
+chsh -s $(which zsh)
 RUNZSH="no" | sh ohmyzsh.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
