d299108ed4307af64b84767c65728a64e4a8269b
huuduc8404 <huuduc8404@gmail.com>
2024-03-20 17:03:17 +0000

    update script

--- install.sh
+++ install.sh
@@ -3,14 +3,12 @@ set -x
 rm -rf /home/gitpod/.oh-my-zsh
 cd /home/gitpod/.dotfiles
 # cd /workspace/dotfiles
-RUNZSH="no" | ./ohmyzsh.sh
+RUNZSH="no" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-cp -f .zshrc ~/.zshrc
-mkdir -p /tmp/gitpod-gitpod-code-zsh/
-cp -f .p10k.zsh /tmp/gitpod-gitpod-code-zsh/.p10k.zsh
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-brew install -q kubectx
-sudo aws/install
-
+# cp -f .zshrc ~/.zshrc
+# mkdir -p /tmp/gitpod-gitpod-code-zsh/
+# cp -f .p10k.zsh /tmp/gitpod-gitpod-code-zsh/.p10k.zsh
